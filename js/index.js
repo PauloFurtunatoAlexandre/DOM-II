91820d072d356327e043c43ff66b5ccc6fc50904
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-05-08 20:49:04 -0500

    added mouseover and keydown events

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
-// Your code goes here
-function firstcommit();
+let logo = document.querySelector('.logo-heading')
+logo.addEventListener('mouseover', () => {
+    logo.style.transform = 'scale(1.2)'
+    logo.style.transform = 'rotate(360deg)'
+    logo.style.transition = 'all 0.3s'
+})
+
+let body = document.querySelector('body')
+body.addEventListener('keydown', () => {
+    body.style.backgroundColor = '#fbe8da'
+    body.style.transition = 'all 0.9s'
+})
+
