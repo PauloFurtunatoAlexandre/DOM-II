b37af9ee279705f218a1cdf083b3b547bab1c8f8
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-05-08 22:23:53 -0500

    added drag n drop event

--- js/index.js
+++ js/index.js
@@ -12,10 +12,59 @@ body.addEventListener('keydown', () => {
 })
 
 let imgBus = document.querySelector('.container.home .intro img')
+
 imgBus.addEventListener('wheel', (e) => {
     event.preventDefault()
 
     const upsideDown = Math.sign(event.deltaY);
 
     imgBus.style.transform = `scale(${upsideDown})`
+    imgBus.style.transition = 'all 0.6s'
 })
+
+let pickElement = document.querySelector('section.content-destination h2')
+console.log(pickElement)
+
+let destination = document.querySelector('section.content-destination img')
+destination.setAttribute('class', 'destination')
+
+var dragged;
+pickElement.addEventListener("drag", function(event) {
+
+}, false);
+
+pickElement.addEventListener("dragstart", function(event) {
+  dragged = event.target;
+  event.target.style.transform = 'scale(1.1)'
+  event.target.style.transition = 'all 0.6s'
+}, false);
+
+pickElement.addEventListener("dragend", function(event) {
+  event.target.style.transform = "scale(1)";
+}, false);
+
+pickElement.addEventListener("dragover", function(event) {
+  event.preventDefault();
+//   event.target.style.color = '#fff'
+}, false);
+
+pickElement.addEventListener("dragenter", function(event) {
+  if (event.target.className == "destination") {
+    event.target.style.background = "black";
+  }
+}, false);
+
+pickElement.addEventListener("dragleave", function(event) {
+  if (event.target.className == "destination") {
+    event.target.style.background = "";
+  }
+}, false);
+
+document.addEventListener("drop", function(event) {
+  event.preventDefault();
+  if (event.target.className == "destination") {
+    event.target.style.background = "";
+    dragged.parentNode.removeChild( dragged );
+    event.target.appendChild( dragged );
+  }
+}, false);
